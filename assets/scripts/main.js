66afe90e67c1718d7e4a764bb5bee0d10d135b67
ThiagoAguirre <thiago.aguirre08@outlook.com>
2024-12-04 19:15:35 -0300

    fix: website

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,13 +1,35 @@
-document.addEventListener("scroll", function() {
-    const elementos = document.querySelectorAll(".animar");
-    const windowHeight = window.innerHeight;
+(function () {
+    document.addEventListener("scroll", function () {
+        const elementos = document.querySelectorAll(".animar");
+        const windowHeight = window.innerHeight;
 
-    elementos.forEach(function(elemento) {
-        const distanciaTopo = elemento.getBoundingClientRect().top;
+        elementos.forEach(function (elemento) {
+            const distanciaTopo = elemento.getBoundingClientRect().top;
 
-        // Se o elemento está dentro da janela de visualização
-        if (distanciaTopo < windowHeight - 100) {
-            elemento.classList.add("ativo");
-        }
+            if (distanciaTopo < windowHeight - 100) {
+                elemento.classList.add("ativo");
+            }
+        });
     });
-});
+
+    $(document).ready(function () {
+        $('.toggle-button').on('click', function () {
+            const button = $(this);
+            const content = button.siblings('.more-text');
+
+            if (content.css('-webkit-line-clamp') === '3' || content.css('max-height') === 'calc(1.5em * 3)') {
+                content.css({
+                    '-webkit-line-clamp': 'unset',
+                    'max-height': 'none'
+                });
+                button.text('Saiba menos');
+            } else {
+                content.css({
+                    '-webkit-line-clamp': '3',
+                    'max-height': 'calc(1.5em * 3)'
+                });
+                button.text('Saiba mais');
+            }
+        });
+    });
+})();
