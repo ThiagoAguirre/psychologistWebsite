6d6fb470c6da7494a421730d44999820f4d5f654
ThiagoAguirre <thiago.aguirre08@outlook.com>
2025-01-12 00:57:07 -0300

    fix: the new function for the FAQs button

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,47 +1,45 @@
 (function () {
-    document.addEventListener("scroll", function () {
-        const elementos = document.querySelectorAll(".animar");
-        const windowHeight = window.innerHeight;
-
-        elementos.forEach(function (elemento) {
-            const distanciaTopo = elemento.getBoundingClientRect().top;
-
-            if (distanciaTopo < windowHeight - 100) {
-                elemento.classList.add("ativo");
-            }
-        });
-    });
-
     $(document).ready(function () {
-        $('.toggle-button').on('click', function () {
-            const button = $(this);
-            const content = button.siblings('.more-text');
+      $('.toggle-button').on('click', function () {
+          const button = $(this);
+          const content = button.siblings('.more-text');
 
-            if (content.css('-webkit-line-clamp') === '3' || content.css('max-height') === 'calc(1.5em * 3)') {
-                content.css({
-                    '-webkit-line-clamp': 'unset',
-                    'max-height': 'none'
-                });
-                button.text('Saiba menos');
-            } else {
-                content.css({
-                    '-webkit-line-clamp': '3',
-                    'max-height': 'calc(1.5em * 3)'
-                });
-                button.text('Saiba mais');
-            }
-        });
-    });
-    let li = document.querySelectorAll(".faq-text li");
-    for (var i = 0; i < li.length; i++) {
-      li[i].addEventListener("click", (e)=>{
-        let clickedLi;
-        if(e.target.classList.contains("question-arrow")){
-          clickedLi = e.target.parentElement;
-        }else{
-          clickedLi = e.target.parentElement.parentElement;
-        }
-       clickedLi.classList.toggle("showAnswer");
+          if (content.css('-webkit-line-clamp') === '3' || content.css('max-height') === 'calc(1.5em * 3)') {
+              content.css({
+                  '-webkit-line-clamp': 'unset',
+                  'max-height': 'none'
+              });
+              button.text('Saiba menos');
+          } else {
+              content.css({
+                  '-webkit-line-clamp': '3',
+                  'max-height': 'calc(1.5em * 3)'
+              });
+              button.text('Saiba mais');
+          }
       });
-    }
+  });
+
+    
+  let li = document.querySelectorAll(".faq-text li");
+
+  for (var i = 0; i < li.length; i++) {
+    li[i].addEventListener("click", (e) => {
+      let clickedLi;
+
+      // Identifica o elemento clicado
+      if (e.target.classList.contains("question-arrow")) {
+        clickedLi = e.target.parentElement;
+      } else {
+        clickedLi = e.target.parentElement.parentElement;
+      }
+
+      // Remove a classe 'showAnswer' de todos os itens
+      li.forEach(item => item.classList.remove("showAnswer"));
+
+      // Adiciona a classe ao item clicado
+      clickedLi.classList.toggle("showAnswer");
+    });
+  }
+
 })();
